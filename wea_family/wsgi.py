49b1ee0219d0e6e938659c6f64cabbe31a1db461
Lizyx <lizyx_971010@163.com>
2021-02-01 23:55:33 +0800

    init

--- wea_family/wsgi.py
+++ wea_family/wsgi.py
@@ -1,6 +1,4 @@
 from flask import g, request
-from json import loads
-from base64 import decodebytes
 
 from wea_family.factory import create_app
 from wea_family.config import get_config
@@ -10,13 +8,7 @@ app = create_app(get_config())
 
 @app.before_request
 def set_g_attribute():
-    data = request.headers.get('X-AMS-User', as_bytes=True)
-    g.x_ams_user = data
-    userinfo = loads(decodebytes(data))
-    g.current_user = userinfo['userId']
-
-    workspace_id = request.headers.get('X-AMS-Workspace')
-    g.workspace = workspace_id
+    ...
 
 
 if __name__ == '__main__':
